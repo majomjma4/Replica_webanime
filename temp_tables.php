693747c65e916d4719ad219da690c1d0dcaef662
Maria José <monterosmarijo2004@gmail.com>
2026-04-12 20:28:04 -0500

    Chore: Clean up legacy tmp files, unused scripts, and backup folders from root directory

--- temp_tables.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'c:\xampp\htdocs\WebAnime_CI4_Replica\app\Config\bootstrap.php';
-require 'c:\xampp\htdocs\WebAnime_CI4_Replica\app\ReplicaCi4\Models\Database.php';
-
-$db = new \ReplicaCi4\Models\Database();
-$c = $db->getConnection(false);
-$tables = $c->query('SHOW TABLES')->fetchAll(PDO::FETCH_COLUMN);
-echo json_encode($tables);
